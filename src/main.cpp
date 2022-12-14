2325c048aac93a63fc6dc10c724e0ad201a02a9b
Siraphong <hideyo99@hotmail.com>
2022-12-14 13:55:59 -0500

    Q3

--- src/main.cpp
+++ src/main.cpp
@@ -180,22 +180,22 @@ int main(void)
     ::g_pTextureManager->setBasePath(TEXTURE_PATH);
     ::g_pTextureManager->create2DTextureFromBMP("Dungeons_2_Texture_01_A.bmp");
     
-    //std::string load_texture_error = "";
-    //if (g_pTextureManager->createCubeTextureFromBMP("SpaceBox",
-    //    "SpaceBox_right1_posX.bmp", /* positive X */
-    //    "SpaceBox_left2_negX.bmp",  /* negative X */
-    //    "SpaceBox_top3_posY.bmp",    /* positive Y */
-    //    "SpaceBox_bottom4_negY.bmp",  /* negative Y */
-    //    "SpaceBox_front5_posZ.bmp",  /* positive Z */
-    //    "SpaceBox_back6_negZ.bmp", /* negative Z */
-    //    true, load_texture_error))
-    //{
-    //    std::cout << "Loaded the tropical sunny day cube map OK" << std::endl;
-    //}
-    //else
-    //{
-    //    std::cout << "ERROR: Didn't load the tropical sunny day cube map.->" << load_texture_error << std::endl;
-    //}
+    std::string load_texture_error = "";
+    if (g_pTextureManager->createCubeTextureFromBMP("SpaceBox",
+        "SpaceBox_right1_posX.bmp", /* positive X */
+        "SpaceBox_left2_negX.bmp",  /* negative X */
+        "SpaceBox_top3_posY.bmp",    /* positive Y */
+        "SpaceBox_bottom4_negY.bmp",  /* negative Y */
+        "SpaceBox_front5_posZ.bmp",  /* positive Z */
+        "SpaceBox_back6_negZ.bmp", /* negative Z */
+        true, load_texture_error))
+    {
+        std::cout << "Loaded the tropical sunny day cube map OK" << std::endl;
+    }
+    else
+    {
+        std::cout << "ERROR: Didn't load the tropical sunny day cube map.->" << load_texture_error << std::endl;
+    }
     //setup object
     //result = pVAOManager->setInstanceObjVisible("terrain01", true);
    // result = pVAOManager->setInstanceObjRGB("floor", glm::vec4(1.f,1.f,1.f,1.f));
